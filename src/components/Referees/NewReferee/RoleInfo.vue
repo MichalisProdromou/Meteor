87d057dcd3d2b6c61a2610b6b1b5af90439c10dc
Michalis Prodromou <michalis.p@statarebrands.com>
2020-03-22 22:59:15 +0200

    changes on add referee functionality

--- src/components/Referees/NewReferee/RoleInfo.vue
+++ src/components/Referees/NewReferee/RoleInfo.vue
@@ -1,10 +1,50 @@
 <template>
-  <h2>Role_Info_Referee</h2>
+  <v-card flat>
+    <v-card-text>
+      <v-form ref="newRefereeRoleInfoForm">
+        <v-row>
+          <v-col>
+            <v-menu
+              v-model="showRoleMenu"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              transition="scale-transition"
+              offset-y
+              min-width="290px"
+            >
+              <template v-slot:activator="{ on }">
+                <v-text-field
+                  v-model="healthCertEndDate"
+                  label="Health certificate expiry date"
+                  outlined
+                  append-icon="mdi-calendar"
+                  readonly
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker 
+                v-model="healthCertEndDate"
+                @input="showRoleMenu = false">
+              </v-date-picker>
+            </v-menu>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
+const moment = require("moment");
+
 export default {
-  name: "Role_Info_Referee"
+  name: "Role_Info_Referee",
+  data(){
+    return {
+      healthCertEndDate: moment(new Date()).format("YYYY-MM-DD"),
+      showRoleMenu: false
+    }
+  }
 
 }
 </script>
